b3c413eeaeea2b1af96200759c706cffbbe051bd
fdebrus <33791533+fdebrus@users.noreply.github.com>
2025-07-11 12:56:31 +0200

    Update auth.py

--- pyaquarite/auth.py
+++ pyaquarite/auth.py
@@ -46,10 +46,34 @@ class AquariteAuth:
             self.client = FirestoreClient(project="hayward-europe", credentials=self.credentials)
 
     async def get_client(self):
-        """Return an authenticated Firestore client (ensure authenticated first)."""
-        if not self.client:
+        if not self.client or not self.tokens:
             await self.authenticate()
+        else:
+            # Check if token is expiring within the next minute
+            if self.expiry - datetime.datetime.now() < datetime.timedelta(seconds=60):
+                await self.refresh_token()
         return self.client
 
     async def close(self):
         await self.session.close()
+
+    async def refresh_token(self):
+        url = f"{self.TOKEN_URL}?key={self.api_key}"
+        data = {
+            "grant_type": "refresh_token",
+            "refresh_token": self.tokens['refreshToken']
+        }
+        async with self.session.post(url, data=data) as resp:
+            if resp.status != 200:
+                raise AuthenticationError("Failed to refresh token.")
+            refreshed = await resp.json()
+            self.tokens['idToken'] = refreshed['id_token']
+            self.tokens['refreshToken'] = refreshed['refresh_token']
+            self.tokens['expiresIn'] = refreshed['expires_in']
+            self.expiry = datetime.datetime.now() + datetime.timedelta(seconds=int(self.tokens["expiresIn"]))
+            # Update credentials object as well
+            self.credentials = Credentials(
+                token=self.tokens['idToken'],
+                refresh_token=self.tokens['refreshToken'],
+                token_uri=self.TOKEN_URL
+            )
